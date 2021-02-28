a8a0889f6f1afd54487ea63cfa916aca4a899540
Andrew Redden <opensource@andrewredden.com>
2021-02-28 05:29:00 -0400

    updates for the package manifest, avatar, docker-entrypoint and added the releases.json

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -3,9 +3,13 @@ set -e
 
 FILE=/radicle-seed/secret.key
 
-if [[ ! -f $FILE ]]; then
-  echo 'No secret found, auto generating one into default location: /radicle-seed/secret.key'
-  (cd /radicle/key-util -- --filename /radicle-seed/secret.key)
+if [[ ! -z $1 ]]; then
+  FILE=$1
 fi
 
-bash -c "/radicle/seed-node < /radicle-seed/secret.key  --root /radicle-seed --peer-listen ${HOST}:12345 --http-listen ${HOST}:8888 --name ${NAME} --assets-path seed/ui/public"
+if [[ ! -f $FILE ]]; then
+  echo "No secret found, auto generating one into default location: $FILE"
+  bash -c "/radicle/key-util --filename $FILE"
+fi
+
+bash -c "/radicle/seed-node --root /radicle-seed --peer-listen ${HOST}:12345 --http-listen ${HOST}:8888 --name ${NAME} --assets-path seed/ui/public < /radicle-seed/secret.key"
