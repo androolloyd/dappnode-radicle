d37a6b2d6e0f586e38f513d1acf9158a9b2a8d73
Andrew Redden <opensource@andrewredden.com>
2021-03-01 23:19:00 -0400

    0.1.0 RC

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
-set -e
+set -ex
 
-FILE=/radicle-seed/secret.key
+SECRET_KEY_FILE=/radicle-seed/secret.key
 
-if [[ ! -z $1 ]]; then
-  FILE=$1
+NODE_PEER_PORT=10001
+NODE_SERVICE_PORT=8888
+ROOT_SEED_DATA=/radicle/seed
+
+SEED_NODE_BINARY=/radicle/seed-node
+
+DOCKER_HOST=0.0.0.0
+
+if [[ -n $1 ]]; then
+  SECRET_KEY_FILE=$1
 fi
 
-if [[ ! -f $FILE ]]; then
-  echo "No secret found, auto generating one into default location: $FILE"
-  bash -c "/radicle/key-util --filename $FILE"
+if [[ -n $LOG_LEVEL ]]; then
+  LOG_LEVEL=$LOG_LEVEL
+else
+  LOG_LEVEL=debug
 fi
 
-bash -c "/radicle/seed-node --root /radicle-seed --peer-listen ${HOST}:12345 --http-listen ${HOST}:8888 --name ${NAME} --assets-path seed/ui/public < /radicle-seed/secret.key"
+if [[ ! -f $SECRET_KEY_FILE ]]; then
+  echo "No secret found, auto generating one into default location: ${SECRET_KEY_FILE}"
+  bash -c "/radicle/key-util --filename ${SECRET_KEY_FILE}"
+fi
+
+if [[ -n $_DAPPNODE_GLOBAL_HOSTNAME ]]; then
+  PUBLIC_ADDRESS=$_DAPPNODE_GLOBAL_HOSTNAME
+fi
+
+if [[ -n $PUBLIC_ADDRESS ]]; then
+ PUBLIC_CMD="--public-addr ${PUBLIC_ADDRESS}:${NODE_PEER_PORT}"
+fi
+
+
+bash -c "${SEED_NODE_BINARY} --root ${ROOT_SEED_DATA} --log ${LOG_LEVEL} --peer-listen ${DOCKER_HOST}:${NODE_PEER_PORT} --http-listen ${DOCKER_HOST}:${NODE_SERVICE_PORT} ${PUBLIC_CMD} --name ${NAME}  --assets-path /radicle/seed/ui/public < ${SECRET_KEY_FILE}"
